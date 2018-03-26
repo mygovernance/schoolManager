a82fe6600b91a86062e8ce29bbfadf0afc7f8e9a
Saddam <saddam.nitjsr@gmail.com>
2018-03-27 05:26:01 +0530

    1.create address class field and their getter and setter. 2. create Assingment class fileds and their getter and setter.

--- SchoolManager/app/src/main/java/com/example/aquibjawed/schoolmanager/Address.java
+++ SchoolManager/app/src/main/java/com/example/aquibjawed/schoolmanager/Address.java
@@ -5,4 +5,63 @@ package com.example.aquibjawed.schoolmanager;
  */
 
 public class Address {
+    private int node_id,state_id,country_id;
+    private String address_line_1,address_line_2,pinccode,city;
+    public int getNode_id() {
+        return node_id;
+    }
+
+    public void setNode_id(int node_id) {
+        this.node_id = node_id;
+    }
+
+    public int getState_id() {
+        return state_id;
+    }
+
+    public void setState_id(int state_id) {
+        this.state_id = state_id;
+    }
+
+    public int getCountry_id() {
+        return country_id;
+    }
+
+    public void setCountry_id(int country_id) {
+        this.country_id = country_id;
+    }
+
+    public String getAddress_line_1() {
+        return address_line_1;
+    }
+
+    public void setAddress_line_1(String address_line_1) {
+        this.address_line_1 = address_line_1;
+    }
+
+    public String getAddress_line_2() {
+        return address_line_2;
+    }
+
+    public void setAddress_line_2(String address_line_2) {
+        this.address_line_2 = address_line_2;
+    }
+
+    public String getPinccode() {
+        return pinccode;
+    }
+
+    public void setPinccode(String pinccode) {
+        this.pinccode = pinccode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+
 }
--- SchoolManager/app/src/main/java/com/example/aquibjawed/schoolmanager/Assignment.java
+++ SchoolManager/app/src/main/java/com/example/aquibjawed/schoolmanager/Assignment.java
@@ -5,4 +5,46 @@ package com.example.aquibjawed.schoolmanager;
  */
 
 public class Assignment {
+    int node_id,class_id;
+    String title,file_url,image_url;
+
+    public int getNode_id() {
+        return node_id;
+    }
+
+    public void setNode_id(int node_id) {
+        this.node_id = node_id;
+    }
+
+    public int getClass_id() {
+        return class_id;
+    }
+
+    public void setClass_id(int class_id) {
+        this.class_id = class_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFile_url() {
+        return file_url;
+    }
+
+    public void setFile_url(String file_url) {
+        this.file_url = file_url;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
 }
