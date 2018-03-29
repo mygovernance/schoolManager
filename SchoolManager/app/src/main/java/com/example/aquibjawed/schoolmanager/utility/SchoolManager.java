45ee616125cfe7ad029138edf5f6b710c0b0d61e
Saddam <saddam.nitjsr@gmail.com>
2018-03-29 21:29:01 +0530

    Implemented school manager class

--- SchoolManager/app/src/main/java/com/example/aquibjawed/schoolmanager/utility/SchoolManager.java
+++ SchoolManager/app/src/main/java/com/example/aquibjawed/schoolmanager/utility/SchoolManager.java
@@ -1,6 +1,8 @@
 package com.example.aquibjawed.schoolmanager.utility;
 
 import com.example.aquibjawed.schoolmanager.School;
+import com.example.aquibjawed.schoolmanager.Student;
+import com.example.aquibjawed.schoolmanager.UpdateUI;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -18,10 +20,6 @@ public class SchoolManager {
     // this class is singleton
     private static SchoolManager schoolManager=null;
 
-    private List<School> schools=null;
-    private School school=null;
-
-
     public static SchoolManager getInsance(){
         if(schoolManager==null){
             schoolManager = new SchoolManager();
@@ -34,71 +32,53 @@ public class SchoolManager {
         // schools is stored in schools on getSchoolList call
     }
 
-    public List<School> createSchool(String response){
-        List<School> schoollist=new ArrayList<School>();
-        return schoollist;
-    }
-
-    public List<School> getSchoolList(){
-        ResponseManager responseManager = new ResponseManager(URLManager.getSchoolURL(), new ProcessFinish(){
-            @Override
-            public void onResponseReceived(String response) {
-                // create schoollist
-                schools=null;
-                schools= createSchoolList(response);
-            }
-        });
-
-        // may return null , because it is in different thread.
-        return schools;
-    }
-
-    public School getSchool(int node_id){
-        school=null;
-        String url = URLManager.getSchoolURL()+node_id;
-        ResponseManager responseManager = new ResponseManager(url, new ProcessFinish() {
-            @Override
-            public void onResponseReceived(String response) {
-                school= createSchoolList(response).get(0);
-            }
-        });
-        return school;
-    }
-
-    private List<School> createSchoolList(String response) {
-        List<School> schools = new ArrayList<School>();
-
+    private List<School> createSchool(String response){
+        List<School> schoolList=new ArrayList<>();
         try {
-            JSONArray schools_json = new JSONArray(response);
-            for (int i = 0; i < schools_json.length(); i++) {
-                // for each school.
-                School school = null;
-                int node_id = -1;
-                String id = null;
-                String registration_id = null;
-                String board_id = null;
-                String address_id = null;
-                String name;
-
-                // getting all the field of a school.
-                JSONObject school_json = schools_json.getJSONObject(i);
-                node_id = Integer.parseInt(school_json.getString("node_id"));
-
-                //  Id is string need to change in future;
-                id = school_json.getString("id");
-                registration_id = school_json.getString("registration_id");
-                board_id = school_json.getString("board_id");
-                name = school_json.getString("name");
-                address_id = school_json.getString("address_id");
-
-                // new School
-                school = new School(node_id, id, registration_id, board_id, address_id);
-                schools.add(school);
+            JSONArray json = new JSONArray(response);
+            int n=json.length();
+            for(int i=0;i<n;i++){
+                int node_id,address_id,board_id;
+                String name=null,id=null,registration_id=null;
+                JSONObject object=json.getJSONObject(i);
+                node_id=Integer.parseInt(object.getString("node_id"));
+                id=object.getString("id");
+                name=object.getString("name");
+                address_id=Integer.parseInt(object.getString("address_id"));
+                board_id=Integer.parseInt(object.getString("board_id"));
+                School school=new School(node_id,id,registration_id,board_id,address_id);
+                schoolList.add(school);
+                school.print();
             }
-        } catch (JSONException e) {
-            //
+
+        }catch (JSONException e){
+            ///
         }
-        return schools;
+        return schoolList;
     }
+
+    public void getSchoolList(final UpdateUI updateUI){
+        ResponseManager responseManager=new ResponseManager(URLManager.getSchoolURL(), new ProcessFinish() {
+            @Override
+            public void onResponseReceived(String response) {
+                List<School> schoolList=createSchool(response);
+                updateUI.updateUI(schoolList);
+            }
+        });
+    }
+
+    public void getSchool(int node_id_of_school,final UpdateUI updateUI){
+        ResponseManager responseManager=new ResponseManager(URLManager.getSchoolURL()+node_id_of_school, new ProcessFinish() {
+            @Override
+            public void onResponseReceived(String response) {
+                List<School> schoolList=createSchool(response);
+                if(schoolList!=null && schoolList.size()>0)
+                updateUI.updateUI(schoolList.get(0));
+                else
+                    updateUI.updateUI(null);
+            }
+        });
+    }
+
 
 }
