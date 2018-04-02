df689c4a967d4166492ac3f946a05ded97e75614
Saddam <saddam.nitjsr@gmail.com>
2018-04-03 01:11:32 +0530

    1.added TAG for log 2. added print function in address class

--- SchoolManager/app/src/main/java/com/example/aquibjawed/schoolmanager/Address.java
+++ SchoolManager/app/src/main/java/com/example/aquibjawed/schoolmanager/Address.java
@@ -1,10 +1,13 @@
 package com.example.aquibjawed.schoolmanager;
 
+import com.example.aquibjawed.schoolmanager.utility.MyLog;
+
 /**
  * Created by saddam on 15/3/18.
  */
 
 public class Address {
+    private final String TAG="Address";
     private int node_id,state_id,country_id;
     private String address_line_1,address_line_2,pinccode,city;
 
@@ -17,7 +20,9 @@ public class Address {
         this.pinccode = pinccode;
         this.city = city;
     }
-
+   public void print(){
+       MyLog.d(TAG," node_id="+node_id+" state_id="+state_id+" country_id="+country_id+" address_line_1="+address_line_1+" address_line_2="+address_line_2+" pinccode="+pinccode+" city="+city);
+   }
     public int getNode_id() {
         return node_id;
     }
