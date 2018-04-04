7e2e55921a3a7c7a7b34987ef8657ee28534fbd3
Saddam <saddam.nitjsr@gmail.com>
2018-04-04 21:08:57 +0530

    changed package name

--- SchoolManager/app/src/main/java/com/example/aquibjawed/schoolmanager/UpdateUI.java
+++
@@ -1,9 +0,0 @@
-package com.example.aquibjawed.schoolmanager;
-
-/**
- * Created by saddam on 27/3/18.
- */
-
-public interface UpdateUI {
-    public void updateUI(Object object);
-}
--- SchoolManager/app/src/main/java/com/example/aquibjawed/schoolmanager/utility/ProcessFinish.java
+++
@@ -1,9 +0,0 @@
-package com.example.aquibjawed.schoolmanager.utility;
-
-/**
- * Created by saddam on 16/3/18.
- */
-
-public interface ProcessFinish {
-    public void onResponseReceived(String response);
-}
