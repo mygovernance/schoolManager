02a6c4668e5e2b167e642fede2176a1585b1643d
Saddam <saddam.nitjsr@gmail.com>
2018-04-12 18:29:40 +0530

    added doubt class and their manager

---
+++ SchoolManager/androidapp/src/main/java/com/school/manager/app/Doubt.java
@@ -0,0 +1,8 @@
+package com.school.manager.app;
+
+/**
+ * Created by saddam on 12/4/18.
+ */
+
+public class Doubt {
+}
---
+++ SchoolManager/androidapp/src/main/java/com/school/manager/app/utility/DoubtManager.java
@@ -0,0 +1,8 @@
+package com.school.manager.app.utility;
+
+/**
+ * Created by saddam on 12/4/18.
+ */
+
+public class DoubtManager {
+}
